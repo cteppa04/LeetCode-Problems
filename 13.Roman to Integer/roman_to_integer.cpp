06309c56af1577e9b4e6096562d4cf37b62f3343
Stefan Kleckow(cteppa04) <kleckovstepan7@gmail.com>
2025-11-14 17:54:18 +0100

    Solution for Leetcode problem: 13.Roman to Integer.

--- 13.Roman to Integer/roman_to_integer.cpp
+++ 13.Roman to Integer/roman_to_integer.cpp
@@ -1,8 +1,68 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
 class Solution 
 {
 public:
+    int getNum(char ch)
+    {
+        switch (ch)
+        {
+        case 'I': return 1;
+            break;
+        case 'V': return 5;
+            break;
+        case 'X': return 10;
+            break;
+        case 'L': return 50;
+            break;
+        case 'C': return 100;
+            break;
+        case 'D': return 500;
+            break;
+        case 'M': return 1000;
+            break;
+        default: return 0;
+            break;
+        }
+    }
     int romanToInt(string s) 
     {
-        
+        int result = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (getNum(s[i]) >= getNum(s[i + 1]))
+            {
+                result += getNum(s[i]);
+            }
+            else
+            {
+                result -= getNum(s[i]);
+            }
+        }
+        return result;
     }
 };
+
+int main()
+{
+    string str;
+    int result;
+    Solution sol;
+
+    str = "III";
+    result = sol.romanToInt(str);
+    cout << str << " = " << result << " in integer\n";
+
+    str = "LVIII";
+    result = sol.romanToInt(str);
+    cout << str << " = " << result << " in integer\n";
+
+    str = "MCMXCIV";
+    result = sol.romanToInt(str);
+    cout << str << " = " << result << " in integer\n";
+
+    return 0;
+}
