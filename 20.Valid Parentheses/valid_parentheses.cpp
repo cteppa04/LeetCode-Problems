5ee3812ebdb43e1e6d2691022026273ce3a973ce
Stefan Kleckow(cteppa04) <kleckovstepan7@gmail.com>
2025-11-18 18:26:03 +0100

    Intialize LeetCode problem 20.Valid Parentheses

---
+++ 20.Valid Parentheses/valid_parentheses.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    bool isValid(string s) 
+    {
+        return false;
+    }
+};
+
+int main()
+{
+    return 0;
+}
